e9d181761e6b0a990408fc52290f4ce1606ab88e
devyosse <narutoshippuden884@gmail.com>
2021-11-18 14:47:34 +0100

    exo 74

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,16 @@
 //Votre code ci dessous
 
+let element = document.getElementById('cat-pic');
 
+
+function miaou(){
+    document.getElementById('cat-chat').innerHTML = '<span>Miaou</span>';
+
+}
+
+element.addEventListener('click', miaou)
+
+miaou()
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,11 +1,15 @@
 let libButton = document.getElementById('lib-button');
+
 let libIt = function() {
+    let none = document.getElementById('noun').value;
+    let adjective = document.getElementById('adjective').value;
+    let name= document.getElementById('person').value;
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    storyDiv.innerHTML = none + " " + adjective + " " + name;
 };
 libButton.addEventListener('click', libIt);
 
-
+let input = document.getElementsByTagName('input');
 
 
 
